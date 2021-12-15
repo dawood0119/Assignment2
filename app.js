22fbbec4f4c96a9e23edff5be2127b808e4d761d
owais17 <muhammad.owais17>
2021-12-15 21:01:48 +0500

    commit

---
+++ app.js
@@ -0,0 +1,20 @@
+
+
+var firstName = prompt("enter first name");
+var lastName = prompt("enter last name");
+var fullName = (firstName +' '+ lastName);
+alert(fullName);
+
+
+
+var favMob = "my favourite mobile is :"
+var myMobName = "Iphone 12 pro mAX";
+var allValue = (favMob +' '+myMobName);
+var lengthOfUser = "length of string :"
+var lengthOfText = myMobName.length;
+var allValueLength = (lengthOfUser+' '+lengthOfText);
+
+document.write(allValue + '<br>' + allValueLength ) ;
+
+var 
+
